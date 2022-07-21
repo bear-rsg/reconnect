cff13bb31875af436922cf925a8aee721d692599
Cerys Lewis (Advanced Research Computing) <C.Lewis.2@bham.ac.uk>
2022-07-21 16:32:34 +0100

    more config changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -170,7 +170,7 @@ MENUITEMS = (
     ('Projects', '/reconnect/projects-gallery.html'),
     ('Reports', '/reconnect/reports.html'),
     ('Resources', '/reconnect/resources.html'),
-    ('Blog', '/reconnect/blog//blog-index.html'),
+    ('Blog', '/reconnect/blog-index.html'),
 )
 
 LOCALE = ('en_GB', 'en')
