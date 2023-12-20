57adce73d50b33dbdda1c1642144788676818d1a
Cerys Lewis (Advanced Research Computing) <C.Lewis.2@bham.ac.uk>
2023-12-20 09:50:22 +0000

    updates to content/design

--- content/js/init.js
+++ content/js/init.js
@@ -10,7 +10,7 @@ skel.init({
 		global:		{ range: '*', href: '/reconnect/css/style.css', containers: 1400, grid: { gutters: 50 } },
 		wide:		{ range: '-2400', href: '/reconnect/css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
 		normal:		{ range: '-1280', href: '/reconnect/css/style-normal.css', containers: 960, lockViewport: true },
-		narrow:		{ range: '-1024', href: '/reconnect/css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
+		narrow:		{ range: '-1042', href: '/reconnect/css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
 		narrower:	{ range: '-840', href: '/reconnect/css/style-narrower.css', grid: { collapse: 1 } },
 		mobile:		{ range: '-640', href: '/reconnect/css/style-mobile.css', containers: '100%', grid: { gutters: 15, collapse: 2 } }
 	}
