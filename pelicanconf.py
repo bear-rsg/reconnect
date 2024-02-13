a97906919ce6478ef4e6e17ddf54273e5302e5a9
Cerys Lewis <c.lewis.2@bham.ac.uk>
2024-02-13 16:43:00 +0000

    fix for broken build

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,8 +30,8 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # PATH_METADATA = 'articles/(?P<path>.*)\..*'
-DIRECT_TEMPLATES = ['index', 'updates', 'resources', 'projects_gallery', 'categories', 'tags', 'team', 'page_with_banner']
-PAGINATED_TEMPLATES = ['index', 'updates', 'resources', 'projects_gallery', 'categories', 'tags', 'page_with_banner']
+DIRECT_TEMPLATES = ['index', 'updates', 'resources', 'projects_gallery', 'categories', 'tags', 'page_with_banner']
+PAGINATED_DIRECT_TEMPLATES = ['index', 'updates', 'resources', 'projects_gallery', 'categories', 'tags', 'page_with_banner']
 
 ARTICLE_URL = 'updates/{slug}'
 ARTICLE_SAVE_AS = 'updates/{slug}/index.html'
